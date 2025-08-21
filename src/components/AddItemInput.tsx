3f8f931566ed17b9b45d64d2645fbafa121a27c8
mateus <mateusweslley14@gmail.com>
2025-08-21 10:37:09 -0300

    Revert "Merge pull request #3 from Toteck/feat/adicionar-Input-controlado"
    This reverts commit 96057bfdc053356bbf5c6e5fc8245f6f1d591c55, reversing
    changes made to 924aedc1953b97e87b5de9df228c1a31ce14c6a7.

--- src/components/AddItemInput.tsx
+++ src/components/AddItemInput.tsx
@@ -1,46 +1,20 @@
 //componente pra adicionar item pra lista//
 
-import React, { useState } from 'react'; from 'react';
+import React from 'react';
 import { TextField, InputAdornment, IconButton } from '@mui/material';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-
-// Define the Task type
-export type Task = {
-  id: string;
-  title: string;
-  completed: boolean;
-};
 
 type AddItemInputProps = {
   onAddItem: (item: string) => void;
 };
 
 const AddItemInput: React.FC<AddItemInputProps> = ({ onAddItem }) => {
-const [title, setTitle] = useState('');
-const handleSubmit = (e: React.FormEvent) => {
-const handleSubmit = (e: React.FormEvent) => { // Adicionado handler
-    e.preventDefault();
-
-    if (!title.trim()) return; // Validação
-
-    const newTask: Task = {   // Cria objeto Tarefa
-      id: crypto.randomUUID(),
-      title: title.trim(),
-      completed: false,
-    };
-
-    onAddItem(newTask);
-    setTitle(''); // Limpa input
-  };
-  
   return (
-    <form onSubmit={ (e) => { e.preventDefault(); onAddItem('Adicionar item a lista');}}>
+    <form onSubmit={ (e) => { e.preventDefault(); onAddItem('teste');}}>
     <TextField
     fullWidth
     placeholder="+ adicionar item da lista"
     variant="standard"
-    value={title
-    onChange={(e) => setTitle(e.target.value)}
     InputProps={{
       startAdornment: (
         <InputAdornment position="start">
