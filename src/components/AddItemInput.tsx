1fa382f374797cd16ad83f0643cda32ebab7f1a8
mateuswdo <53489827+Toteck@users.noreply.github.com>
2025-08-22 17:29:49 +0000

    merge(fix/form-e-atualiza-estado-input): resolver conflitos em AddItemInput

--- src/components/AddItemInput.tsx
+++ src/components/AddItemInput.tsx
@@ -1,6 +1,5 @@
-//componente pra adicionar item pra lista//
-
-import React, { useState } from 'react'; // adicionando useState
+// componente pra adicionar item pra lista //
+import React, { useState } from 'react';
 import { TextField, InputAdornment, IconButton } from '@mui/material';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
@@ -11,39 +10,39 @@ export type Task = {
 };
 
 type AddItemInputProps = {
-  onAddItem: (title: string) => void;
+  onAddItem: (title: string) => void; 
 };
 
 const AddItemInput: React.FC<AddItemInputProps> = ({ onAddItem }) => {
   const [title, setTitle] = useState('');
 
-  const handleSubmit = (e: React.FormEvent) => { // adicionado handler
+  const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (!title.trim()) return; // validação
+    if (!title.trim()) return;
 
-    onAddItem(title.trim()); // envia apenas a string
-    setTitle(""); // limpa o input após adicionar
-    };
+    onAddItem(title.trim()); 
+    setTitle('');
+  };
 
   return (
-    <form onSubmit={handleSubmit}> {/*adicionando o form para capturar o submit*/}
-    <TextField
-    fullWidth
-    placeholder="+ adicionar item da lista"
-    variant="standard"
-    value={title}
-    onChange={(e) => setTitle(e.target.value)} // atualiza o estado do input
-    InputProps={{
-      startAdornment: (
-        <InputAdornment position="start">
-          <IconButton type="submit">
-            <AddCircleOutlineIcon />
-          </IconButton>
-        </InputAdornment>
-      ),
-    }}
-  />
-</form>
+    <form onSubmit={handleSubmit}>
+      <TextField
+        fullWidth
+        placeholder="+ adicionar item da lista"
+        variant="standard"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)} 
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <IconButton type="submit">
+                <AddCircleOutlineIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+    </form>
   );
 };
 
