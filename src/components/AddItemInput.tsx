0569f0668ed412bafca6c90f3d4564e09c0e1783
mateuswdo <53489827+Toteck@users.noreply.github.com>
2025-08-22 19:17:25 +0000

    fix: update code the branch to code in the main

--- src/components/AddItemInput.tsx
+++ src/components/AddItemInput.tsx
@@ -1,9 +1,9 @@
-//componente pra adicionar item pra lista//
-
-import React, { useState } from 'react'; // adicionando useState
+// componente pra adicionar item pra lista //
+import React, { useState } from 'react';
 import { TextField, InputAdornment, IconButton } from '@mui/material';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
+// Estrutura de task
 export type Task = {
   id: string;
   title: string;
@@ -17,33 +17,33 @@ type AddItemInputProps = {
 const AddItemInput: React.FC<AddItemInputProps> = ({ onAddItem }) => {
   const [title, setTitle] = useState('');
 
-  const handleSubmit = (e: React.FormEvent) => { // adicionado handler
+  const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (!title.trim()) return; // validação
+    if (!title.trim()) return;
 
-    onAddItem(value.trim()); // envia apenas a string
-    setTitle(""); // limpa o input após adicionar
-    };
+    onAddItem(title.trim());
+    setTitle('');
+  };
 
   return (
-    <form onSubmit={handleSubmit}> // adicionando o form para capturar o submit
-    <TextField
-    fullWidth
-    placeholder="+ adicionar item da lista"
-    variant="standard"
-    value={title}
-    onChange={(e) => setTitle(e.target.value)} // atualiza o estado do input
-    InputProps={{
-      startAdornment: (
-        <InputAdornment position="start">
-          <IconButton type="submit">
-            <AddCircleOutlineIcon />
-          </IconButton>
-        </InputAdornment>
-      ),
-    }}
-  />
-</form>
+    <form onSubmit={handleSubmit}>
+      <TextField
+        fullWidth
+        placeholder="+ adicionar item da lista"
+        variant="standard"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <IconButton type="submit">
+                <AddCircleOutlineIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+    </form>
   );
 };
 
