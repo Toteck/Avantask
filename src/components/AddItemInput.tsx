f122980ddf9c6002155363b278907b16624face0
mateus <mateusweslley14@gmail.com>
2025-08-22 15:38:24 -0300

    Revert "Merge pull request #5 from Toteck/feat/adicionar-Input-controlado"
    This reverts commit a84a31ac8d92fa7199ba85b542f5c0c094560831, reversing
    changes made to 3f8f931566ed17b9b45d64d2645fbafa121a27c8.

--- src/components/AddItemInput.tsx
+++ src/components/AddItemInput.tsx
@@ -1,54 +1,32 @@
 //componente pra adicionar item pra lista//
 
-import React, { useState } from 'react'; // adicionando useState
+import React from 'react';
 import { TextField, InputAdornment, IconButton } from '@mui/material';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-
-export type Task = {
-  id: string;
-  title: string;
-  completed: boolean;
-};
 
 type AddItemInputProps = {
   onAddItem: (item: string) => void;
 };
 
 const AddItemInput: React.FC<AddItemInputProps> = ({ onAddItem }) => {
-  const [title, setTitle] = useState('');
-
-   const handleSubmit = (e: React.FormEvent) => { // adicionado handler
-    e.preventDefault();
-
-    if (!title.trim()) return; // validação
-
-    const newTask: Task = {   // cria objeto Tarefa
-      id: crypto.randomUUID(),
-      title: title.trim(),
-      completed: false,
-    };
-  }
-
- 
-    return (
-      <form onSubmit={(e) => { e.preventDefault(); onAddItem('teste'); }}>
-        <TextField
-          fullWidth
-          placeholder="+ adicionar item da lista"
-          variant="standard"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <IconButton type="submit">
-                  <AddCircleOutlineIcon />
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
-      </form>
-    );
-  };
-
+  return (
+    <form onSubmit={ (e) => { e.preventDefault(); onAddItem('teste');}}>
+    <TextField
+    fullWidth
+    placeholder="+ adicionar item da lista"
+    variant="standard"
+    InputProps={{
+      startAdornment: (
+        <InputAdornment position="start">
+          <IconButton type="submit">
+            <AddCircleOutlineIcon />
+          </IconButton>
+        </InputAdornment>
+      ),
+    }}
+  />
+</form>
+  );
+};
 
 export default AddItemInput;
